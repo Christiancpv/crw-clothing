4d9a3901e448516e24ba63d8b9af4222ef731211
Christiancpv <christian.costapinheiro1@gmail.com>
2020-05-13 11:55:11 -0300

    created user and cart sagas for sign in and sign up with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
+import { googleSignInStart, emailSignInStart} from "../../redux/user/user.actions";
+
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -12,30 +14,26 @@ class SignIn extends React.Component {
 
     this.state = {
       email: "",
-      password: ""
+      password: "",
     };
   }
 
-  handleSubmit = async event => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+    const { emailSignInStart } = this.props
+    const { email, password } = this.state;
 
-    const {email, password} = this.state;
-
-    try{
-      await auth.signInWithEmailAndPassword(email,password)
-      this.setState({ email: "", password: "" });
-    } catch (error) {
-      console.log(error);
-    }
+    emailSignInStart(email,password); 
   };
 
-  handleChange = event => {
+  handleChange = (event) => {
     const { value, name } = event.target;
 
     this.setState({ [name]: value });
   };
 
   render() {
+    const { googleSignInStart } = this.props;
     return (
       <div className="sign-in">
         <h2>I already have an account</h2>
@@ -60,9 +58,13 @@ class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit"> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={googleSignInStart}
+              isGoogleSignIn
+            >
               Sign in with Google
-            </CustomButton>
+            </CustomButton>   
           </div>
         </form>
       </div>
@@ -70,4 +72,8 @@ class SignIn extends React.Component {
   }
 }
 
-export default SignIn;
+const mapDispatchToProps = (dispatch) => ({
+  googleSignInStart: () => dispatch(googleSignInStart()),
+  emailSignInStart: (email,password) => dispatch(emailSignInStart({email, password}))
+});
+export default connect(null,mapDispatchToProps)(SignIn);
---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,37 @@
+import { takeLatest, call, put, all } from "redux-saga/effects";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+import {
+  fetchcollectionsSucess,
+  fetchCollectionsFailure,
+} from "./shop.actions";
+
+import ShopActionTypes from "./shop.types";
+
+export function* fetchCollectionsAsync() {
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchcollectionsSucess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeLatest(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
+
+export function* shopSagas(){
+  yield all([call(fetchCollectionsStart)])
+}
